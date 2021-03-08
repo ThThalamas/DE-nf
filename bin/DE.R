c1dfdf35dac8dc7d029c1ad8b5fd99aa9a078a71
Boris <boris.lipinski83@gmail.com>
2021-03-08 12:37:56 +0100

    add figure on Rscript

--- bin/DE.R
+++ bin/DE.R
@@ -19,7 +19,6 @@ split <- function(name){name <- str_split(name,"_")[[1]][1]}
 ###############################################################################################################################################
 ## -- DESeq2 annalyse -- ##
 ###########################
-# DEA
 DEA_analysis <- function(matrix, metadata){
   #load
   countData <- read.table(matrix, row.names = 1, header = T, sep = "\t")
@@ -40,9 +39,9 @@ DEA_analysis <- function(matrix, metadata){
   colData[1] <- apply(colData[1], 1, remplace)
   colData <- colData[order(colData[,1]),]
   
-  #intersect with data laready processed
+  #intersect with data already processed
   colData <- colData[which(colData$`Name of sample` %in% intersect(colData$`Name of sample`, colnames(countData))),]
-  #colData <- colData[which(colData$`Sex`=='Female'),]
+  colData <- colData[which(colData$`Sex`=='Female'),]
   
   countData <- countData[,which(colnames(countData) %in% intersect(colData$`Name of sample`, colnames(countData)))]
   
@@ -54,26 +53,10 @@ DEA_analysis <- function(matrix, metadata){
 
   return(dataset)
 }
-#matrix <- "/home/boris/Bureau/projet/DE-nf/result/finale.txt"
-#metadata <- "/home/boris/Bureau/projet/DE-nf/data/Metadata.xls"
-
-matrix = as.character(commandArgs(TRUE)[1])
-metadata = as.character(commandArgs(TRUE)[2])
+matrix <- "/home/boris/Bureau/projet/DE-nf/result/finale.txt" #matrix = as.character(commandArgs(TRUE)[1])
+metadata <- "/home/boris/Bureau/projet/DE-nf/data/Metadata.xls" #metadata = as.character(commandArgs(TRUE)[2])
 dataset <- DEA_analysis(matrix, metadata)
 rld <- rlog(dataset, blind = T)
-
-
-
-# PCA
-pdf("/home/boris/Bureau/projet/DE-nf/result/Result_PCA.pdf")
-P <- plotPCA(rld, intgroup = "Modality")
-P + theme_bw() + ggtitle("Rlog transformed counts")
-
-P <- plotPCA(rld, intgroup = "Sex")
-P + theme_bw() + ggtitle("Rlog transformed counts")
-dev.off()
-
-
 
 # Top Genes Results
 DEA_result <- function(dataset, group, reference, group2){
@@ -96,3 +79,35 @@ sex_result <- DEA_result(dataset, "Sex", "Female", "Male")
 topGenes_sex <- assay(rld)[rownames(sex_result),]
 
 
+###############################################################################################################################################
+## -- Figure time -- ##
+#######################
+pdf("/home/boris/Bureau/projet/DE-nf/result/Result.pdf")
+
+# Dendrogramme
+rlog.norm.counts <- assay(rld)
+distance.m_rlog  <- as.dist(1 - cor(rlog.norm.counts , method = "pearson" ))
+plot(hclust(distance.m_rlog), labels = colnames(rlog.norm.counts),main = "rlog  transformed  read  counts\ndistance: Pearson  correlation")
+
+# PCA
+plotPCA(rld, intgroup = "Modality") + theme_bw() + ggtitle("Rlog transformed counts")
+plotPCA(rld, intgroup = "Sex")+ theme_bw() + ggtitle("Rlog transformed counts")
+
+# aheatmap
+aheatmap(topGenes_modality, Colv = TRUE, hclustfun = "average", scale = "row", main="By modality") 
+aheatmap(topGenes_sex, Colv = TRUE, hclustfun = "average", scale = "row", main="By sex") 
+#top40Genes <- head(assay(rld)[rownames(topGenes_modality),], 40)
+#aheatmap(top40Genes, Colv = TRUE, hclustfun = "average", scale = "row")
+
+# Plot count
+topGene <- rownames(modality_result)[which.min(modality_result[,"padj"])]
+els <- plotCounts(dataset, gene = topGene, intgroup="Modality", returnData=TRUE)
+els$sex = dataset@colData$Sex
+ggplot(els,aes(x = Modality, y = count)) + ggtitle(topGene) + geom_point(aes(color = sex))
+
+# PlotMA
+plotMA(topGenes_sex, main = "MA plot: group1 vs group2 (padj < 0.05)", ylim = c(-4,4))
+abline(h = c(-1,1), col = "blue", lty = 2)
+mtext(c(paste("-2 fold"), paste("+ 2 fold")), side = 4, at = c(-1, 1), cex = 0.8, line = 0.5, col = "blue")
+
+dev.off()
--- bin/figure.R
+++ bin/figure.R
@@ -22,36 +22,16 @@ View(topResults)
 dim(topResults)
 
 # Figure time :
-#aheatmap
-top40Genes <- head(assay(rld)[rownames(topResults),], 40)
-aheatmap(top40Genes, Colv = TRUE, hclustfun = "average", scale = "row")
-aheatmap(topGenes, Colv = TRUE, hclustfun = "average", scale = "row") 
+
 
 # Histograme of p_values
 hist(result$pvalue, col = "grey", border = "white", xlab = "", ylab = "", main = "frequencies  of p-values")
 hist(result$pvalue[result$baseMean > 1], col = "grey", border = "white", xlab = "", ylab = "", main = "frequencies  of p-values")
 
-# PlotMA
-plotMA(topGenes, main = "MA plot: group1 vs group2 (padj < 0.05)", ylim = c(-4,4))
-abline(h = c(-1,1), col = "blue", lty = 2)
-mtext(c(paste("-2 fold"), paste("+ 2 fold")), side = 4, at = c(-1, 1), cex = 0.8, line = 0.5, col = "blue")
-
-# Dendrogramme
-rlog.norm.counts <- assay(rld)
-distance.m_rlog  <- as.dist(1 - cor(rlog.norm.counts , method = "pearson" ))
-plot(hclust(distance.m_rlog), labels = colnames(rlog.norm.counts),main = "rlog  transformed  read  counts\ndistance: Pearson  correlation")
 
 
-# Plot count
-topGene <- rownames(result)[which.min(result$padj)]
-els <- plotCounts(dataset, gene = topGene, intgroup="group", returnData=TRUE)
-els$sex = c("F","F","F","F","F","F","F","M","M","M","M","M","M","M",
-            "F","F","F","F","F","F","F","M","M","M","M","M","M","M",
-            "F","F","F","F","M","M","M","M",
-            "F","F","F","F","F","F","F","M","M","M","M","M","M","M")
 
-ggplot(els,aes(x = group, y = count)) + ggtitle(topGene) + 
-  geom_point(aes(color = sex))
+
 
 # Other
 plotDispEsts(dataset)
